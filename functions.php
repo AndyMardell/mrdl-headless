460f95edb9ecc388dad2d60f09288c680d5959e1
Andy Mardell <andy@mardell.me>
2018-08-21 10:29:02 +0100

    Remove unwanted title from sprintf

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ add_filter( 'excerpt_length', 'modify_excerpt_length', 999 );
 function modify_excerpt_more( $link ) {
 	$link = sprintf( '<p class="more-link-container"><a href="/%1$s" class="more-link">%2$s</a></p>',
 		get_post_field( 'post_name', get_post() ),
-		sprintf( __( 'read more' ), get_the_title( get_the_ID() ) )
+		'read more'
 	);
 	return ' &hellip; ' . $link;
 }
