3183aefbb2eeda8f677e80fda95a93616f504c51
Andy Mardell <andy@mardell.me>
2018-08-21 10:27:07 +0100

    Return relative post links

--- functions.php
+++ functions.php
@@ -6,12 +6,8 @@ function modify_excerpt_length( $length ) {
 add_filter( 'excerpt_length', 'modify_excerpt_length', 999 );
 
 function modify_excerpt_more( $link ) {
-	if ( is_admin() ) {
-		return $link;
-	}
-
-	$link = sprintf( '<p class="more-link-container"><a href="%1$s" class="more-link">%2$s</a></p>',
-		esc_url( get_permalink( get_the_ID() ) ),
+	$link = sprintf( '<p class="more-link-container"><a href="/%1$s" class="more-link">%2$s</a></p>',
+		get_post_field( 'post_name', get_post() ),
 		sprintf( __( 'read more' ), get_the_title( get_the_ID() ) )
 	);
 	return ' &hellip; ' . $link;
