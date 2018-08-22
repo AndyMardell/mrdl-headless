c74210ebe23743956ebf97cd5fcd2f370e5fb46c
Andy Mardell <andy@mardell.me>
2018-08-22 11:16:01 +0100

    Ditch the read more button

--- functions.php
+++ functions.php
@@ -6,10 +6,6 @@ function modify_excerpt_length( $length ) {
 add_filter( 'excerpt_length', 'modify_excerpt_length', 999 );
 
 function modify_excerpt_more( $link ) {
-	$link = sprintf( '<p class="more-link-container"><a href="/%1$s" class="more-link">%2$s</a></p>',
-		get_post_field( 'post_name', get_post() ),
-		'read more'
-	);
-	return ' &hellip; ' . $link;
+	return ' &hellip; ';
 }
 add_filter( 'excerpt_more', 'modify_excerpt_more' );
