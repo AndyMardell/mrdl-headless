4c8b189590d07ed67b8c0898696b3b510eb265c9
Andy Mardell <andy@sidigital.co>
2018-08-30 23:40:56 +0100

    Adds nav manu

--- functions.php
+++ functions.php
@@ -9,3 +9,10 @@ function modify_excerpt_more( $link ) {
 	return ' &hellip; ';
 }
 add_filter( 'excerpt_more', 'modify_excerpt_more' );
+
+function add_nav_menus() {
+	register_nav_menus([
+		'main-menu' => 'Main Menu'		  
+	]);
+}
+add_action( 'init', 'add_nav_menus' );
